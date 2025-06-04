4db0737fc9ca34e478b65c890909901b2b0afbda
TristanK117 <81729665+TristanK117@users.noreply.github.com>
2025-06-03 21:47:07 -0700

    firebase linked

--- Snip-d/Snip_dApp.swift
+++ Snip-d/Snip_dApp.swift
@@ -4,14 +4,29 @@
 //
 //  Created by Tristan Khieu on 6/3/25.
 //
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Snip_dApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
